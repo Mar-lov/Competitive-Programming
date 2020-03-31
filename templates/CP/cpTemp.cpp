9cb414eea4dc4b36e9485f1b565f0e8701300e90
Martin <Martin@Martins-MacBook-Pro.local>
2020-03-30 23:02:16 -0500

    added new files

---
+++ templates/CP/cpTemp.cpp
@@ -0,0 +1,35 @@
+/*
+Code by @marlov       
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <utility>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pi;
+
+
+
+
+int main() {
+
+    return 0;
+}
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1,n=0?)
+	* do smth instead of nothing and stay organized
+*/
