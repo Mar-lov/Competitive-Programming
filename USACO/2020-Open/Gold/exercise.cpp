9677624d52da27f0abfacec6d878b5f11a9c9f57
Martin <Martin@martins-mbp.attlocal.net>
2020-07-27 11:10:01 -0500

    added more problems

---
+++ USACO/2020-Open/Gold/exercise.cpp
@@ -0,0 +1,43 @@
+/*
+Code by @marlov       
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <utility>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <iterator>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pi;
+
+#define maxN 10005
+
+int N;
+int MOD;
+int dp[maxN];
+int main() {
+	cin>>N>>MOD;
+	dp[1]=1;
+	for(int i=2;i<=N;i++){
+		
+	}
+    return 0;
+}
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1,n=0?)
+	* do smth instead of nothing and stay organized
+*/
